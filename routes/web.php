dd45a9fbccfe62f3885e4d710ceb790cd53d98c4
UsamaUmer109 <66095107+UsamaUmer109@users.noreply.github.com>
2023-11-21 19:24:47 +0500

    Make Layout For User View
    Make Layout For User View

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+// For Guest User
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
+Route::get('/user', [UserController::class, 'index']);
 
-Route::get('/user', [UserController::class, 'index'])->middleware('user');
-Route::get('/admin', [AdminController::class, 'index'])->middleware('admin');
+Route::middleware(['user'])->group(function () {
+    // Route::get('/user', [UserController::class, 'index']);
+});
+Route::middleware(['user'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index']);
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
